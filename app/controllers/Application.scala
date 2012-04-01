774db0c030c97026c9f14151437150c352e55e22
Jared Roesch <roeschinc@gmail.com>
2012-03-31 23:28:11 -0700

    fixed login, and logout

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,14 +10,28 @@ import com.mongodb.casbah.Imports._
 object Application extends Controller {
   
   def index = Action { request =>
+    val uid = request.session.get("userid")
+    uid match {
+      case Some(id) => {
+        val query = MongoDBObject("_id" -> new ObjectId(id))
+        val user = UserDAO.findOne(query)
+        Ok(views.html.index(user.get))
+      }
+      
+      case None => {
+        Ok(views.html.index(User(username = "Guest", password = "", salt = "")))
+      }
+    }
+  }
+  
+  def newPost = Action { request =>
     val uid = request.session.get("userid").get
     val query = MongoDBObject("_id" -> new ObjectId(uid))
     val user = UserDAO.findOne(query)
-    Ok(views.html.index(user.get))
-  }
-  
-  def newPost = Action {
-    Ok(views.html.createPost())
+    if (user.get.admin)
+      Ok(views.html.createPost())
+    else 
+      Redirect("/") //redirect here
   }
   
   def login = Action { implicit request =>
@@ -31,8 +45,15 @@ object Application extends Controller {
       }
   
       case "POST" => {
-        val (user, pw) = loginForm.bindFromRequest.get
-        Ok(user + pw)
+        val (username, pw) = loginForm.bindFromRequest.get
+        val query = MongoDBObject("username" -> username)
+        val user = UserDAO.findOne(query).get
+        if (user.password == pw) 
+          Redirect("/").withSession {
+            "userid" -> user._id.toString
+          }
+        else 
+          Redirect("/login")
       }
     }
   }
