f5a76eaf58922f07cc9205410c73cef8b2c09f4b
Jared Roesch <roeschinc@gmail.com>
2012-04-03 21:44:18 -0700

    hacked editing to work for now

--- app/controllers/Posts.scala
+++ app/controllers/Posts.scala
@@ -7,6 +7,8 @@ import play.api.data.Forms._
 import eu.henkelmann.actuarius.ActuariusTransformer
 import models._
 import com.mongodb.casbah.Imports._
+import com.novus.salat._
+import com.novus.salat.global._
 
 object Posts extends Controller {
   private val guest = User(username = "Guest", password = "", salt = "")
@@ -56,11 +58,13 @@ object Posts extends Controller {
   }
   
   def update(id: String) = Action { implicit request =>
+    println("updating!")
     val (author, title, content) = postForm.bindFromRequest.get
     val md = transformer(content)
-    val post: Post = Post(title = title, content = md, author = author)
-    val _id = PostDAO.insert(post)
-    Redirect("/posts/"+(_id.get.toString))
+    val updatedPost = Post(title = title, content = md, author = author)
+    val oldPost = PostDAO.findOne(MongoDBObject("_id" -> new ObjectId(id))).get
+    PostDAO.update(grater[Post].asDBObject(updatedPost), grater[Post].asDBObject(oldPost))
+    Redirect("/posts/"+id)
   }
   
   def delete(id: String) = Action {
